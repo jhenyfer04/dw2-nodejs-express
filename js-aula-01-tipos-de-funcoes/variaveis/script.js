fbe6d172739e4804fd249f884bc1a1651273a705
DSM2 <fatec-dsm2@academico.intra>
2026-02-11 21:22:54 -0300

    aula 01 variaveis

---
+++ js-aula-01-tipos-de-funcoes/variaveis/script.js
@@ -0,0 +1,28 @@
+// comentario de linha
+
+/*comentário de bloco */
+
+// Variaveis popdem ser declaradas de 3 formas:
+// VAR, LET E CONST
+
+// VAR: no geral evite o seu o uso, pode nao ser muito seguro.
+// LET:utilize quando for necessario reatribuir o valor da variável.
+// CONST: utilize quando NÃO precisar reatribuir o valor da variável.
+
+var cidade = "Registro";
+var cidade = "Sete Barras";
+
+document.write(cidade);
+
+let nome = "cibelly";
+let nome = "maria";
+ 
+document.write("<br>");
+document.write(nome);
+
+const pais = "Brasil";
+pais = "Argentina";
+
+// O valor de uma consatante nao pode ser alçterado
+document.write("<br>");
+document.write(pais);
