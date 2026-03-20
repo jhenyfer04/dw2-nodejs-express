54596708b7dc01616ffb26b50b78d62d06db4eb1
DSM2 <fatec-dsm2@academico.intra>
2026-03-20 18:12:55 -0300

    aula03.3 - array objeto

---
+++ nodejs-aula-03.2-parametros-de-rotas/index.js
@@ -0,0 +1,78 @@
+// O arquivo index.js e o arquivo principal do projeto
+
+// Importando o MODULO do express
+const express = require ("express")
+
+// Criando uma instancia do express
+const app = express();
+
+//configurando o EJS
+app.set('view engine', 'ejs');
+
+//criando a rota principal do site("/")
+app.get("/", function(req, res){
+  //  res.send("bem-vindo ao meu primeiro site em Node.js e Express.js");
+ res.render("index");
+})
+
+//criando a rota de perfil do usuario
+app.get("/perfil", (req, res) =>{
+   // res.send("Bem-vindo ao seu perfil!");
+ res.render("perfil");
+});
+
+// rota de Clientes
+app.get("/clientes", (req, res) => {
+  const listaCliente = ["maria", "joao", "zezinho", "vitor"];
+  res.render("clientes", {
+    listaCliente: listaCliente,
+  });
+});
+
+// rota de clientes-com parametros
+app.get("/clientes/:cliente", (req, res) => {
+  const cliente = req.params.cliente;
+  res.render("detalhesCliente", {
+    cliente: cliente,
+  });
+});
+
+
+// rota de produtos
+app.get("/produtos",  (req, res)=>{
+   const listaProdutos =['computador', 'celular', 'tablet', 'notebook'];
+    res.render("produtos",{
+        // enviando variaveis para a pagina de HTML
+     listaProdutos : listaProdutos  
+})
+});
+
+
+// rota de produtos-com parametros
+app.get("/produtos/:produto",  (req, res)=>{
+ const produto = req.params.produto;
+    res.render("detalhesProduto",{
+    produto: produto,    
+    });
+});
+
+app.get("/servicos", (req, res)=>{
+ res.render("servicos");
+});
+
+
+// Iniciando o servidor  na porta 8080
+const port = 8080
+app.listen(port, (error) => {
+    if(error){
+        console.log("ocorreu um erro ao iniciar o servidor!" + error);
+    } else {
+        console.log(`servidor iniciado com sucesso na endereço : http://localhost:${port}`);
+    }
+});
+
+
+
+
+
+
